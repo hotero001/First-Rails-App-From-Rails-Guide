bcb51ad235c76068435e993661403ef3a4a1ad7f
Hector Otero <otero111@hotmail.com>
2015-02-09 22:29:42 -0800

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	has_many :comments, dependent: :destroy
-	validates :title, presence: true,
-										length: { minimum: 3 }
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+  length: { minimum: 3 }
 end
